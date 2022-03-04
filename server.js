6b3fc8f0cd155a7a1cf5353b8133f2a70b2fa621
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-03-03 22:07:29 -0700

    finilized api routes and htmlroutes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require('./routes/apiRoutes');
-require('./routes/htmlRoutes');
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
