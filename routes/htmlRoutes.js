64ba980663d68e60cb04ae1a568fe0e549a3887b
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-03-16 15:56:32 -0600

    formatted and touched the server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,10 @@
-// Dependencies
 const path = require("path");
 
-// Routing
-
-module.exports = function (app){
-
-    // HTML GET Requests
-    // Below code handles when users 'visit' a page.
-    // In each of the below cases the user is shown an HTML page of content
-
-    
-    app.get('/notes', function(request, response) {
-        response.sendFile(path.join(__dirname, '../public/notes.html'));
+module.exports = function (app) {
+    app.get('/notes', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-
-    app.get('*', function(request, response) {
-        response.sendFile(path.join(__dirname, '../public/index.html'));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
